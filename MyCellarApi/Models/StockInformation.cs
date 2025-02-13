9a99846c2972449946242110bd556401735ca713
Luca Morgado <luca.morgado.pro@gmail.com>
2025-02-13 17:14:35 +0100

    added Cellar, WineBottle and StockInformation models

---
+++ MyCellarApi/Models/StockInformation.cs
@@ -0,0 +1,23 @@
+﻿using MyCellarApiCore.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCellarApi.Models
+{
+    public class StockInformation : BaseModel
+    {
+        [Required]
+        public int WineId { get; set; }
+
+        [ForeignKey(nameof(WineBottle))]
+        public virtual WineBottle WineBottle { get; set; }
+
+        [Required]
+        public int CellarId { get; set; }
+
+        [ForeignKey(nameof(Cellar))]
+        public virtual Cellar Cellar { get; set; }
+        [Required]
+        public int Quantity { get; set; } = 0;
+}
+}
