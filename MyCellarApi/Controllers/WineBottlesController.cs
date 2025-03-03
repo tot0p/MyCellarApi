0e568bc8847ee536c93f976282c83093a11a0d00
Luca Morgado <luca.morgado.pro@gmail.com>
2025-03-03 11:19:13 +0100

    added controlers for models

---
+++ MyCellarApi/Controllers/WineBottlesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyCellarApi.Data;
+using MyCellarApi.Models;
+using MyCellarApiCore.Controllers;
+
+namespace MyCellarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WineBottlesController : BaseController<MyCellarDbContext, Cellar>
+    {
+        public WineBottlesController(MyCellarDbContext context) : base(context)
+        {
+        }
+    }
+}
