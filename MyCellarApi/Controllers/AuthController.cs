c7ecee618e16beca0a8c203864b7742f8afa5ede
Luca Morgado <luca.morgado.pro@gmail.com>
2025-03-27 09:54:14 +0100

    finished authentication using google openid connect

---
+++ MyCellarApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCellarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            var redirectUrl = Url.Action("LoginCallback", "Auth", null, Request.Scheme);
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            Console.WriteLine(properties.RedirectUri);
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("login-callback")]
+        public IActionResult LoginCallback()
+        {
+            // Handle the callback from the OpenID Connect provider
+            return Ok("Login successful");
+        }
+
+        [Authorize]
+        [HttpGet("profile")]
+        public IActionResult Profile()
+        {
+            var claims = User.Claims.Select(c => new { c.Type, c.Value });
+            return Ok(claims);
+        }
+    }
+}
